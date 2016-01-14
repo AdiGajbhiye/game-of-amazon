d1906ea0baaa2f7a9febc77549063ffab6e31dd9
Pro <aditya.gajbhiye.20@gmail.com>
2016-01-15 01:53:45 +0530

    completed but not working

--- game_v1.py
+++ game_v1.py
@@ -1,8 +1,13 @@
 #display board
 def display_board():
-    for i in range(12):
-        print board[i]
-
+    for i in range(1,11):
+        for j in range(1,11):
+            if board[i][j] == -1:
+                print "x",
+            else:
+                print board[i][j],
+        print    
+    #inp = raw_input()
 
 #generate possible moves
 def generate(x, y):
@@ -89,61 +94,66 @@ def territory(player):
         x,y = player_pos[player][i]/100,player_pos[player][i]%100
         moves = generate(x,y)
         piece.append(len(moves))
-        '''
-        for j in moves:
-            arrow_x = j/100
-            arrow_y = j%100
-            arrow = generate(arrow_x, arrow_y)
-        '''    
     return sum(piece)
 
 
 
 #recursion
 def solve(depth,  turn):
-    ter_1 = territory(0)
-    ter_2 = territory(1)
-    if depth == 5: return ter_1
+    ter_1 = territory(1)   #territory of oposition
+    ter_2 = territory(0)   #minimize 
+    if depth == 3: return 0
     if (ter_1 == 0) or (ter_2 == 0): return ter_1
-    min_score = []
-    min_score_piece = []
+    min_score = 10000
+    min_arrow = 0
+    min_piece_move = 0
+    min_which_piece = -1
+    max_score = -10000
+    max_arrow = 0
+    max_piece_move = 0
+    max_which_piece = -1
     for piece in range(4):
         cur_x,cur_y = player_pos[turn][piece]/100,player_pos[turn][piece]%100
-        score_move = []
         piece_moves = generate(cur_x, cur_y)
-        min_score_arrow = []
         for move in piece_moves:
             board[cur_x][cur_y] = 0
             next_x = move/100
             next_y = move%100
             board[next_x][next_y] = turn + 1
-            score_arrow = []
+            player_pos[turn][piece] = move
             arrow_moves = generate(next_x, next_y)
             for arrow_move in arrow_moves:
                 board[arrow_move/100][arrow_move%100] = -1
-                score_arrow.append(territory(turn))
-                #do something
+                print piece,move,arrow_move
+                display_board()
+                total_territory = solve(depth + 1, (turn + 1)%2)
+                if total_territory <= min_score:
+                    min_score = total_territory
+                    min_arrow = arrow_move
+                    min_piece_move = move
+                    min_which_piece = piece
+                if total_territory >= max_score:
+                    max_arrowscore = total_territory
+                    max_arrow = arrow_move
+                    max_piece_move = move
+                    max_which_piece = piece
                 board[arrow_move/100][arrow_move%100] = 0
-            #index of arrow having min score of a piece move    
-            min_index = score_arrow.index(min(score_arrow))
-            #list of arrow move for min score of a piece move
-            min_score_arrow.append(arrow_moves[min_index])
-            #list of min score of a piece move
-            score_move.append(min(score_arrow))    
             #board reset   
             board[next_x][next_y] = 0
             board[cur_x][cur_y] = turn + 1
-        #list of min score of each piece
-        min_score.append(min(score_move))
-        #index of min score piece
-        min_index = score_move.index(min_score[piece])
-        #list of piece move with min score piece
-        min_score_piece.append(piece_moves[min_index])
-    score = min(min_score)
-    choice[0] = player_pos[turn][min_score.index(score)]
-    choice[1] = 
-    choice[2] = 
+            player_pos[turn][piece] = (100*cur_x) + cur_y
     if turn == 0:
+        choice[0] = min_which_piece
+        choice[1] = min_piece_move
+        choice[2] = min_arrow
+        print choice
+        return min_score
+    else:
+        choice[0] = max_which_piece
+        choice[1] = max_piece_move
+        choice[2] = max_arrow
+        print choice
+        return max_score
         
 
 
@@ -155,16 +165,22 @@ for i in range(12): board[i][0] = 9
 for i in range(12): board[i][11] = 9
 board[1][4] = 2
 board[1][7] = 2
-board[4][1] = 2
-board[4][10] = 2
-board[10][4] = 1
-board[10][7] = 1
+board[4][4] = 2
+board[4][9] = 2
 board[7][1] = 1
+board[7][4] = 1
 board[7][10] = 1
+board[10][8] = 1
+board[2][4] = -1
+board[6][4] = -1
+board[7][9] = -1
+board[10][10] = -1
 
 choice = [0,0,0]
-player_pos = [[ 1004 , 1007 , 701 , 710 ] , [ 104 , 107 , 401 , 410 ] ] 
+player_pos = [[ 701 , 704 , 710 , 1008 ] , [ 104 , 107 , 404 , 409 ] ] 
 
 #main
 display_board()
-solve(1,0)
+print solve(1,0)
+print choice
+display_board()
